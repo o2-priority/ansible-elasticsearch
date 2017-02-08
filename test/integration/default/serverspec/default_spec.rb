7ecb8074ba4e16531d5c42eb3f049ba80fe857f1
Ogonna Iwunze <ogonna.iwunze@gmail.com>
2017-02-08 16:10:33 +0000

    [OI] update integration tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -27,9 +27,7 @@ end
 %W(
   #{elasticsearch_log_dir}
   #{elasticsearch_pid_dir}
-  #{elasticsearch_conf_dir}
   #{elasticsearch_data_dir}
-  #{elasticsearch_home_dir}/plugins
 ).each do |d|
   describe file(d) do
     it { should be_directory }
@@ -39,10 +37,12 @@ end
 end
 
 %W(
+  #{elasticsearch_conf_dir}
   #{elasticsearch_home_dir}
   #{elasticsearch_home_dir}/bin
   #{elasticsearch_home_dir}/lib
   #{elasticsearch_home_dir}/modules
+  #{elasticsearch_home_dir}/plugins
 ).each do |d|
   describe file(d) do
     it { should be_directory }
@@ -51,18 +51,21 @@ end
   end
 end
 
-# check cloud-aws plugin is installed
-describe file("#{elasticsearch_home_dir}/plugins/cloud-aws") do
-  it { should be_directory }
-  it { should be_mode 755 }
-  it { should be_owned_by 'root' }
+# check plugins are installed
+%w(
+  discovery-ec2
+  repository-s3
+).each do |plugin|
+  describe file("#{elasticsearch_home_dir}/plugins/#{plugin}") do
+    it { should be_directory }
+    it { should be_mode 755 }
+    it { should be_owned_by 'root' }
+  end
 end
 
 %W(
   /etc/default/elasticsearch
-  /etc/sysctl.d/60-elasticsearch.conf
   #{elasticsearch_conf_dir}/elasticsearch.yml
-  #{elasticsearch_conf_dir}/logging.yml
 ).each do |f|
   describe file(f) do
     it { should be_file }
@@ -72,8 +75,7 @@ end
 end
 
 describe file('/etc/init.d/elasticsearch') do
-  it { should be_file }
-  it { should be_mode 755 }
+  it { should be_executable }
   it { should be_owned_by 'root' }
 end
 
